f74db5c32b3ef5a7dc2c728cbc7811136a1f2373
Erick Schmitt <ejschmitt@gmail.com>
2010-06-08 11:12:47 -0400

    Add comment to source, including the license and url

--- jquery.html5data.js
+++ jquery.html5data.js
@@ -1,3 +1,12 @@
+/*
+ * html5data jQuery Plugin v1
+ * http://github.com/ejschmitt/html5data
+ *
+ * Copyright (c) 2010 Erick Schmitt
+ * Licensed under the MIT license.
+ *
+ */
+ 
 (function (jQuery) {
   jQuery.fn.extend({
     _data : jQuery.fn.data,
